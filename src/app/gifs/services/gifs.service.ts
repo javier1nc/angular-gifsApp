44b2246c7ad43d5c944b6e63859ea74d3f5dc3f7
Javier Sánchez Mendoza <code@javier1nc.com>
2022-07-11 04:56:45 +0000

    update files

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey    : string = '3y4MHVHvyhBlZExbX7PGDa3zyUKoiWy8';
   private _historial: string[] = [];
+
+  public resultados: any[] = [];
 
   get historial(){
     return [...this._historial];
   }
+
+  constructor( private http: HttpClient){}
 
   buscarGifs( query: string) {
 
@@ -20,7 +27,13 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
     }
 
-    console.log(this._historial);
+    this.http.get<SearchGifsResponse>(`http://api.giphy.com/v1/gifs/search?api_key=3y4MHVHvyhBlZExbX7PGDa3zyUKoiWy8&q=${query}&limit=10`)
+      .subscribe( (resp)=> {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      })
+
   }
+
 
 }
